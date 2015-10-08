bbc444518259b3c27adb2e39cb4093163bf33c27
Fan Yang <fyang2@oakland.edu>
2015-10-08 17:24:39 -0400

    ui-info change

--- Code/materialize-iron-router/server/lib/config/accounts.js
+++ Code/materialize-iron-router/server/lib/config/accounts.js
@@ -54,26 +54,33 @@ Meteor.startup(function() {
   */
 });
 
-var users = [
-      {name:"Normal User",email:"normal@example.com",roles:[]},
-      {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
-      {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']},
-      {name:"Admin User",email:"admin@example.com",roles:['admin']}
-    ];
+if (Meteor.users.find().fetch().length === 0) {
 
-_.each(users, function (user) {
-  var id;
+    console.log('Creating users: ');
 
-  id = Accounts.createUser({
-    email: user.email,
-    password: "apple1",
-    profile: { name: user.name }
-  });
+    var users = [
+        {name:"Normal User",email:"normal@example.com",roles:[]},
+        {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
+        {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']},
+        {name:"Admin User",email:"admin@example.com",roles:['admin']}
+      ];
 
-  if (user.roles.length > 0) {
-    // Need _id of existing user record so this call must come 
-    // after `Accounts.createUser` or `Accounts.onCreate`
-    Roles.addUsersToRoles(id, user.roles, 'default-group');
+    _.each(users, function (userData) {
+      var id,
+          user;
+      
+      console.log(userData);
+
+      id = Accounts.createUser({
+        email: userData.email,
+        password: "apple1",
+        profile: { name: userData.name }
+      });
+
+      // email verification
+      Meteor.users.update({_id: id}, {$set:{'emails.0.verified': true}});
+
+      Roles.addUsersToRoles(id, userData.roles);
+    
+    });
   }
-
-});
