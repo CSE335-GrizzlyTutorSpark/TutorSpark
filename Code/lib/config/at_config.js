81371d56805cc9b95116d1891fadd2a10c3762eb
Fan Yang <fyang2@oakland.edu>
2015-10-24 12:28:01 -0400

    login toast

--- Code/lib/config/at_config.js
+++ Code/lib/config/at_config.js
@@ -1,3 +1,20 @@
+
+var mySubmitFunc = function(error, state){
+  if (!error) {
+    if (state === "signIn") {
+      // Successfully logged in
+      // ...
+        
+        Materialize.toast('Welcome Back', 4000)
+    }
+    if (state === "signUp") {
+      // Successfully registered
+      // ...
+    Materialize.toast('I am a toast!', 4000)
+    }
+  }
+};
+
 // Options
 AccountsTemplates.configure({
     //defaultLayout: 'emptyLayout',
@@ -24,4 +41,10 @@ AccountsTemplates.configure({
     // Privacy Policy and Terms of Use
     //privacyUrl: 'privacy',
     //termsUrl: 'terms-of-use',
+    
+    
+    onSubmitHook: mySubmitFunc,
 });
+
+
+
